7f357b80c6ce059a150511f7e4de61809d5ade8b
Amarildo <you@example.com>
2024-03-11 17:33:01 +0100

    home controller added invoke

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class HomeController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        // Utilizza Eloquent per recuperare tutti i film
+        $movies = Movie::all();
+
+        return view('movies.index', compact('movies'));
+    }
 }
